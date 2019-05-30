0274d8b148824af2b8690fa1fc161625c3a9c9b9
Andrei Komlev <mrluscus@gmail.com>
2019-05-30 16:17:29 +0700

    Tiny change

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,4 +4,4 @@ import "./index.css";
 import App from "./components/App";
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root")); 
